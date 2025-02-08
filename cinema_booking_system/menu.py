945b02efc5ab1dba61c624b4ae155b55ec958b2f
Lohenl <Lohenl@users.noreply.github.com>
2025-02-08 13:21:07 +0800

    updates menu

--- cinema_booking_system/menu.py
+++ cinema_booking_system/menu.py
@@ -1,26 +1,36 @@
-# filepath: /c:/Users/winso/cinema-booking-system-test/cinema_booking_system/menu.py
-from simple_term_menu import TerminalMenu
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
 
 class Menu:
     def __init__(self):
-        self.options = ["Option 1", "Option 2", "Option 3", "Exit"]
+        self.options = ["1", "2", "3"]
+        self.completer = WordCompleter(self.options, ignore_case=True)
 
     def display_menu(self):
-        terminal_menu = TerminalMenu(self.options)
-        menu_entry_index = terminal_menu.show()
-        return menu_entry_index
+        user_input = prompt(
+            "\n"
+            "Welcome to GIC Cinemas\n"
+            "[1] Book Tickets for 'Inception' (64 seats available)\n"
+            "[2] Check Bookings\n"
+            "[3] Exit\n"
+            "\n"
+            "Please enter your selection (Press Tab to view available options):\n"
+            "\n",
+            completer=self.completer
+        )
+        return user_input
 
     def run(self):
         while True:
             choice = self.display_menu()
-            if choice == 0:
-                print("You selected Option 1")
-            elif choice == 1:
-                print("You selected Option 2")
-            elif choice == 2:
-                print("You selected Option 3")
-            elif choice == 3:
-                print("Exiting...")
-                break
-            else:
-                print("Invalid choice, please try again.")
+            match choice:
+                case "1":
+                    print(f"\nBook Tickets for 'Inception' (64 seats available)")
+                case "2":
+                    print("\nCheck Bookings")
+                case "3":
+                    print("\nThank you for using GIC Cinemas System. Bye!")
+                    break
+                case _:
+                    print("\nInvalid choice, please try again.")
+                    
